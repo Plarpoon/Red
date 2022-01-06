79362f1c81f18f0a310facac61c7e4443eec7591
Alex <plarpoon.dev@gmail.com>
2022-01-06 19:17:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Red;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("app-settings.json", true)
+            .Build();
+
+        RunAsync(config).GetAwaiter().GetResult();
+    }
+}
