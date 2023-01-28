e12e0909315f1517b18010df86ed97499777314f
Alex <plarpoon.dev@gmail.com>
2023-01-28 13:35:03 +0100

    base structure

---
+++ InteractionHandler.cs
@@ -0,0 +1,40 @@
+using Discord.Interactions;
+using Discord.webSocket;
+using System.Reflection;
+
+namespace Red
+{
+    public class InteractionHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _commands;
+        private readonly IServiceProvider _services;
+
+        public InteractionHandler(DiscordSocketClient client, InteractionService commands, IServiceProvider services)
+        {
+            _client = client;
+            _commands = commands;
+            _services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModuleAsync(Assembly.GetEntryAssembly(), _services);
+
+            _client.InteractionCreated += HandleInteraction;
+        }
+
+        private async Task HandleInteraction(SocketInteraction arg)
+        {
+            try
+            {
+                var ctx = new SocketInteractionContext(_client, arg);
+                await _commands.ExecuteCommandsAsync(ctx, _services);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.webSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Red
+{
+    public class Program
+    {
+        public static Task Main() => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            using IHost host = host.CreteDefaultBuilder()
+            .ConfigureServices((_, services) =>
+            services
+            .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged,
+                AlwaysDownloadUsers = true,
+            })))
+            .Build();
+
+            await host.RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            using IServiceScope serviceScope = host.Services.ConfigureServices();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            var _client = provider.GetRequiredService<DiscordSocketClient>();
+
+            _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+
+            _client.Ready += async () =>
+            {
+                Console.WriteLine("Bot is connected!");
+            };
+
+            await _client.LoginAsync(TokenType.Bot, "token");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
