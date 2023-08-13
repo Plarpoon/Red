53163329868d2ded3d470f0f54f2c1bfff04b005
Alessandro Suha <plarpoon.dev@gmail.com>
2023-08-13 02:58:42 +0200

    pull out from Log folder as I don't want to chagne .gitignore again

---
+++ LogToFile.cs
@@ -0,0 +1,21 @@
+using Serilog;
+
+namespace EvilBunny.Logs
+{
+    public static class LogToFile
+    {
+        public static void Configure()
+        {
+            // Configure Serilog to write log messages to the console and a file
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: "Logs/EvilLog-" + DateTime.Now.ToString("hhmm-") + ".log",
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    retainedFileCountLimit: null,
+                    shared: true)
+                .CreateLogger();
+        }
+    }
+}
