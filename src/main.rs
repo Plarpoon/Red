732321e8543b1921d3e2655d79cb0e488df6f1e2
Alessandro Suha <github@plarp.net>
2025-01-21 17:28:54 +0100

    fix: now config.toml get created properly

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 mod bot;
 
 use bot::events::handlers::Handler;
+use red::config::Config;
 use serenity::prelude::*;
 use serenity::Client;
 use tokio;
 
 #[tokio::main]
 async fn main() {
-    let token = std::env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Load the configuration file
+    let config = Config::load_or_create("placeholder_token".to_string(), 1)
+        .expect("Failed to load or create config");
+
+    let token = if config.red.token == "placeholder_token" {
+        eprintln!(
+            "No valid token found! Please update 'config.toml' and replace 'placeholder_token' with your actual Discord token."
+        );
+        std::process::exit(1);
+    } else {
+        config.red.token
+    };
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
